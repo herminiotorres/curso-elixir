d319c59b3cfb9e2e8b0e75c2cc7c6d7ceb99c574
Herminio Torres <herminiocesar@gmail.com>
2020-10-09 08:49:02 -0300

    add ex_mon package

---
+++ ex_mon/lib/ex_mon.ex
@@ -0,0 +1,18 @@
+defmodule ExMon do
+  @moduledoc """
+  Documentation for `ExMon`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExMon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
