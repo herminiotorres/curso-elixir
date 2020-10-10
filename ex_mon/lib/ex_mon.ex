6bf8eb20ec20e1a11565bfec316cb08fb201a8f6
Herminio Torres <herminiocesar@gmail.com>
2020-10-10 01:56:10 -0300

    add agents

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,7 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
