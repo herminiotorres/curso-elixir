967407fab4db68f95b9c98cad19a419d4b493a93
Herminio Torres <herminiocesar@gmail.com>
2020-10-13 13:09:18 -0300

    add an extra ex_mon tests

--- ex_mon/test/ex_mon_test.exs
+++ ex_mon/test/ex_mon_test.exs
@@ -7,7 +7,11 @@ defmodule ExMonTest do
 
   describe "create_player/4" do
     test "returns a player" do
-      expected_player = %Player{life: 100, moves: %{move_avg: :high_kick, move_heal: :heal, move_rnd: :punch}, name: "JohnDoe"}
+      expected_player = %Player{
+        life: 100,
+        moves: %{move_avg: :high_kick, move_heal: :heal, move_rnd: :punch},
+        name: "JohnDoe"
+      }
 
       assert expected_player == ExMon.create_player("JohnDoe", :high_kick, :punch, :heal)
     end
@@ -25,6 +29,32 @@ defmodule ExMonTest do
       assert messages =~ "The game is started!"
       assert messages =~ "status: :started"
       assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build("JohnDoe", :high_kick, :punch, :heal)
+
+      capture_io(fn ->
+        assert ExMon.start_game(player) == :ok
+      end)
+
+      # Examples:
+      # {:ok, player: player}
+      :ok
+    end
+
+    test "when the move is valid, do the move and the computer makes a move" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:wrong)
+        end)
+
+      assert messages =~ "Invalid move: wrong"
+    end
+
+    test "when the move is invalid, returns an error message" do
     end
   end
 end
