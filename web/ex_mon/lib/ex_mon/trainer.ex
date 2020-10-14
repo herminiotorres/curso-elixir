97b3a6ffa15c1e5086706653ed8254ab275c32bc
Herminio Torres <herminiocesar@gmail.com>
2020-10-14 18:18:29 -0300

    add a facade pattern to create, read, update and delete trainers

--- web/ex_mon/lib/ex_mon/trainer.ex
+++ web/ex_mon/lib/ex_mon/trainer.ex
@@ -7,7 +7,7 @@ defmodule ExMon.Trainer do
   schema "trainers" do
     field :name, :string
     field :password_hash, :string
-    field :passwrd, :string, virtual: true
+    field :password, :string, virtual: true
     timestamps()
   end
 
@@ -30,6 +30,5 @@ defmodule ExMon.Trainer do
   defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
     change(changeset, Argon2.add_hash(password))
   end
-
   defp put_pass_hash(changeset), do: changeset
 end
