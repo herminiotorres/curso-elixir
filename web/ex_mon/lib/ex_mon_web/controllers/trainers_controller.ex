620f1abb15fbbfcc8ea663616c5c31f4c54a4a94
Herminio Torres <herminiocesar@gmail.com>
2020-10-15 09:12:07 -0300

    add a delete action

--- web/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
+++ web/ex_mon/lib/ex_mon_web/controllers/trainers_controller.ex
@@ -9,11 +9,23 @@ defmodule ExMonWeb.TrainersController do
     |> handle_response(conn)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete_trainer()
+    |> handle_delete(conn)
+  end
+
+  defp handle_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+  defp handle_delete({:error, _reason} = error, _conn), do: error
+
   defp handle_response({:ok, trainer}, conn) do
     conn
     |> put_status(:created)
     |> render("create.json", trainer: trainer)
   end
-
   defp handle_response({:error, _changeset} = error, _conn), do: error
 end
