e1efcf0273c816c7d7f7254fff17fc13f9920dfa
Herminio Torres <herminiocesar@gmail.com>
2020-10-15 08:28:27 -0300

    add the trainers view

---
+++ web/ex_mon/lib/ex_mon_web/views/trainers_view.ex
@@ -0,0 +1,16 @@
+defmodule ExMonWeb.TrainersView do
+  use ExMonWeb, :view
+
+  alias ExMon.Trainer
+  
+  def render("create.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+    %{
+      message: "Trainer created!",
+      trainer: %{
+        id: id,
+        name: name,
+        inserted_at: inserted_at
+      }
+    }
+  end
+end
