725574aac4f0162a5f6fce4519c185e430787330
Herminio Torres <herminiocesar@gmail.com>
2020-10-15 11:38:29 -0300

    add a get pokemon

--- web/ex_mon/lib/ex_mon.ex
+++ web/ex_mon/lib/ex_mon.ex
@@ -1,8 +1,12 @@
 defmodule ExMon do
-  alias ExMon.Trainer
+  alias ExMon.{Pokemon, Trainer}
 
+  # trainer
   defdelegate get_trainer(params), to: Trainer.Get, as: :call
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
   defdelegate update_trainer(params), to: Trainer.Update, as: :call
   defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
+
+  # pokemon
+  defdelegate fetch_pokemon(params), to: Pokemon.Get, as: :call
 end
