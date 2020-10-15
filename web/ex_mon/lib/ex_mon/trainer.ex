756472afe2953424314f26619195aadbcf1a5851
Herminio Torres <herminiocesar@gmail.com>
2020-10-15 14:57:40 -0300

    add the pokemon schema and changeset

--- web/ex_mon/lib/ex_mon/trainer.ex
+++ web/ex_mon/lib/ex_mon/trainer.ex
@@ -2,12 +2,15 @@ defmodule ExMon.Trainer do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias ExMon.Trainer.Pokemon
+
   @primary_key {:id, Ecto.UUID, autogenerate: true}
 
   schema "trainers" do
     field :name, :string
     field :password_hash, :string
     field :password, :string, virtual: true
+    has_many(:pokemon, Pokemon)
     timestamps()
   end
 
